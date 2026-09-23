4c47aeccf03961a868c777ce98b035bf5b97e7f4
Ian Muyumba <ianmandela@gmail.com>
2026-09-23 11:28:09 +0300

    My first Java program

---
+++ week1/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+* My first Java program
+* @author Ian
+* @date Wednesday, September 23, 2026
+*/
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Hello, World!");
+System.out.println("My name is: Ian");
+System.out.println("I am learning Java at Riara");
+System.out.println("Today's date is: Wednesday, September 23, 2026");
+}
+}
